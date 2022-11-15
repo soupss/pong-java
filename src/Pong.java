f179a14714eeda2d43386d48aabf8b5727ea8506
Anton Laurell <antonlaurell@hotmail.com>
2022-11-15 23:32:18 +0100

    moving green ball

--- src/Pong.java
+++ src/Pong.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.animation.AnimationTimer;
 
 public class Pong extends Application {
     public static void main(String[] args) {
@@ -11,10 +14,27 @@ public class Pong extends Application {
 
     @Override
     public void start(Stage stage) {
+        stage.setTitle("Pong");
         Group root = new Group();
-        Scene scene = new Scene(root, 800,600);
-        scene.setFill(Color.GREEN);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
+
+        Canvas canvas = new Canvas(800, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+
+        final long startNanoTime = System.nanoTime();
+
+        new AnimationTimer() {
+            public void handle(long currentNanoTime) {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+                gc.setFill(Color.WHITE);
+                gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                gc.setFill(Color.GREEN);
+                gc.fillOval(t*25, 100, 80, 80);
+            }
+        }.start();
+
         stage.show();
     }
 }
